995c53ba8f6516555337f764cf686db5ee366766
Asuka <asuka@ptzcam.local>
2026-05-06 10:43:58 +0800

    chore: 更新版本号至 V 0.13.506_f90d69a By Asuka

--- ptzcam-qt6/app/utils/constants.py
+++ ptzcam-qt6/app/utils/constants.py
@@ -10,7 +10,7 @@ DEFAULT_HEIGHT: int = 850
 
 # Version info
 VERSION: str = "0.13.506"
-VERSION_STRING: str = f"V {VERSION}_f42f30a By Asuka"
+VERSION_STRING: str = f"V {VERSION}_f90d69a By Asuka"
 
 # Colors
 COLOR_BG_MAIN: str = "#fff"
